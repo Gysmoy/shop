9906482b2ebd61ac574140aa6e9086f32d89ef81
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-12-30 18:09:14 -0500

    gambo

--- assets/js/getGeneralConfig.js
+++ assets/js/getGeneralConfig.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+    // Estableciendo reloj en tiempo real
     moment.locale('es');
     setInterval(() => {
         var time = moment().format('LTS');
         $('#realtime').text(time);
     }, 1000);
+
+    // Obteniendo configuración general
     var idPage = $('body').attr('page');
     console.log(idPage);
+    $.ajax({
+        url: 'api/config/' + idPage,
+        type: 'GET',
+        dataType: 'JSON',
+        headers: {
+
+        },
+        success: res => {
+            console.log(res);
+        },
+        error: e => {
+            console.log(e);
+        }
+    })
 })
