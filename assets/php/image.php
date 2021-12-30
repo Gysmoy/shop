fa70c4e8206fb387c0cc872a5e76abcc983f6ede
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-12-30 17:06:29 -0500

    gambo

--- assets/php/image.php
+++ assets/php/image.php
@@ -1,43 +1,48 @@
 <?php
-if(!isset($_GET['id'])){
+if(
+    isset($_GET['type']) &&
+    isset($_GET['id'])
+){
     require_once 'database.php';
     $db = new Database();
-    /**
-     * Declaramos la consulta donde :nombre_cualquiera es
-     * la posición de la variable que se le pasará en el 
-     * metodo execute()
-     */
-    $query = $db -> connect() -> prepare('SELECT
-        columna_imagen
-    FROM mi_tabla
-    WHERE id = :id_imagen_get
-    ');
-    /**
-     * Le pasamos la variable del id en :id_imagen_get
-     */
-    $query -> execute([
-        ':id_imagen_get' => $_GET['id'],
-        // Pueden ir mas variables según las posiciones
-        // declaradas en la consulta (:var1, :var2, :varx)
-    ]);
-    /**
-     * Obtenemos la primera columna con fetch y todas con fetchAll
-     */
+    switch ($_GET['type']) {
+        case 'logo':
+            $sql = "SELECT 
+                gc.logo_image AS 'image',
+                gc.logo_type AS 'type'
+            FROM general_config gc
+            INNER JOIN business b ON b.id = gc._business
+            WHERE b.path = :path
+            ";
+            $params = [
+                'path' => $_GET['id']
+            ];
+            break;
+        case 'background':
+            $sql = "SELECT 
+                gc.bg_image AS 'image',
+                gc.bg_type AS 'type'
+            FROM general_config gc
+            INNER JOIN business b ON b.id = gc._business
+            WHERE b.path = :path
+            ";
+            $params = [
+                'path' => $_GET['id']
+            ];
+            break;
+        case 'container':
+            # code...
+            break;
+        default:
+            # code...
+            break;
+    }
+    $query = $db -> connect() -> prepare($sql);
+    $query -> execute($params);
     $row = $query -> fetch(PDO::FETCH_ASSOC);
-    // Verificamos la existencia de alguna columna
     if ($row) {
-        header('Content-type: imagen/png');
-        /**
-         * Content-type: imagen/png declara que el contenido que
-         * se mostrará es una imagen png (puede ser jpg también)
-         */
-        echo $row['columna_imagen'];
-        /**
-         * Imprime el base64 (string) de la imagen pero el header
-         * anterior lo codifica como imagen (multimedia)
-         */
-    } else {
-        echo 'La imagen para el id ' . $_GET['id'] . 'No esta disponible';
+        header('Content-Type: ' . $row['type']);
+        echo $row['image'];
     }
 } else {
     echo 'Se require un id para mostrar una imagen';
