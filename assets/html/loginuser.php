e26a8eb41d6e622840eb2099686d13796238d9ba
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-12-31 19:20:01 -0500

    gambo

--- assets/html/loginuser.php
+++ assets/html/loginuser.php
@@ -17,27 +17,34 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Nunito:600,700" rel="stylesheet">
+
   <!-- CSS Libraries -->
   <!--link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"-->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
   <link href="./lib/animate/animate.min.css" rel="stylesheet">
   <link href="./lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
   <link href="../lib/flaticon/font/flaticon.css" rel="stylesheet">
+
   <!-- Template Stylesheet -->
   <link href="../css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="../css/login-singup.css">
 </head>
+
 <body>
   <div class="container">
     <div class="col-md-6 grid-margin stretch-card">
+
+
       <div class="conenedor-form card p-2">
         <div class="card-body">
+
           <form>
             <a href="../../index.php"><img class="logo" src="../img/shop.svg" alt="logo"></a>
             <div class="mb-3 mt-5 text-center">
               <h4>Mozo en linea | Cliente</h4>
               <h6>Iniciar sesíon</h6>
             </div>
+
             <div class="row">
               <div class="col-md-12 mb-1">
               <button id="init-session-facebook" class="btn init-session-btn"> <img
@@ -45,17 +52,20 @@
                 <p class="text-init-facebook">Iniciar sesíon con Facebook</p>
               </button>
               </div>
+
               <div class="col-md-12 mb-3">
               <button id="init-session-google" class="btn init-session-btn"> <img src="../img/google_icon-icons.svg"
                   class=" icon-init-session" alt="icon google">
                 <p class="text-init-google">Iniciar sesíon con Google</p>
               </button>
               </div>
+
               <hr>
+
               <div class="col-md-12">
                 <div class="mb-1">
                   <label class="form-label label-form-login" for="">Correo electrónico / usuario</label>
-                  <input id="email" class="form-control" type="text" placeholder="Correo elecrónico o usuario">
+                  <input id="user" class="form-control" type="text" placeholder="Correo elecrónico o usuario">
                 </div>
               </div>
               <div class="col-md-12">
@@ -64,27 +74,39 @@
                   <input id="pass" class="form-control" type="password" placeholder="********">
                 </div>
               </div>
-              <div id="contVer" class="col-md-12">
-                <div class="mb-1">
-                  <label class="form-label label-form-login" for="">Ingrese codigo de verificasion</label>
-                  <input id="verCod" class="form-control" type="text" placeholder="00000000">
-                </div>
-              </div>
               <div class="label-form-login">
-                <p>Ya tengo cuenta <a href="#">deseo iniciar cecion</a></p>
+                <p>Aun no tengo una cuenta <a href="#">deseo tegistrarme</a></p>
               </div>
               <div class="col-md-12">
                 <div class="mb-1">
-                  <button type="submit" class="btn init-session-shop">Iniciar sesíon</button>
+                  <button id="init-session-shop" class="btn">Iniciar sesíon</button>
                 </div>
               </div>
+
             </div>
+
+           
+
+            
+            
+            
+
+            
+            <div>
+
+
+            </div>
+
           </form>
         </div>
+
       </div>
+
     </div>
   </div>
+
   <div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
       crossorigin="anonymous"></script>
@@ -94,9 +116,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
       integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
       crossorigin="anonymous"></script>
-
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/loginUser.js"></script>
+    <script src="../js/login_src.js"></script>
 
   </div>
 
--- assets/js/loginUser.js
+++ assets/js/loginUser.js
@@ -1,15 +1,15 @@
 
- var codVerification = '';
+var codVerification = '';
 
-$(document).submit(function (e) { 
+$(document).submit(function (e) {
     e.preventDefault();
     var envio = false;
     var request = {}
     request['pass'] = $('#pass').val();
-    request['email'] =  $('#email').val();
+    request['email'] = $('#email').val();
 
     var codVerificasionEmail = '';
-    if(envio == 0){
+    if (envio == 0) {
         codVerification = makeid(8);
         $.ajax({
             method: 'POST',
@@ -17,47 +17,47 @@ $(document).submit(function (e) {
             dataType: 'json',
             accepts: 'application/json',
             data: {
-                name:'Correo de verificasion de SHOP... Queresmos estar seguros de que eres tu ;)',
+                name: 'Correo de verificasion de SHOP... Queresmos estar seguros de que eres tu ;)',
                 message: `Tu codigo de verificasion  de tu Gmail es ===  ${codVerification}  ===`
             },
             success: (data) => {
-                        $('#contVer').show();
-                       
-                        envio = true;
-                    },
+                $('#contVer').show();
+
+                envio = true;
+            },
             error: (err) => console.log(err)
         });
 
-    }else if (envio == true){
+    } else if (envio == true) {
         codVerificasionEmail = $('#verCod').val()
-        if (codVerification == codVerificasionEmail ){
+        if (codVerification == codVerificasionEmail) {
             $.ajax({
                 type: "post",
                 url: "../../api/login.php",
                 data: request,
                 dataType: "json",
-                success: function (response) { 
+                success: function (response) {
                     console.log('Enviado')
                 }
             });
         }
-        
+
     }
 
 
 
-    
-    
 
-    
+
+
+
 });
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
- }
+}
