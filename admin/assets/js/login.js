ba806f785c4ef63e69f26269b8866276e834ea10
Najhely Gutierrez <najhelygutierrezcastro@gmail.com>
2022-01-15 17:34:17 -0500

    naty_

---
+++ admin/assets/js/login.js
@@ -0,0 +1,42 @@
+$('#login').submit(function(form){
+    form.preventDefault();
+    $('#btn-icon').attr('class', 'fa fa-spin fa-spinner');
+    var username = $('#username').val().trim() == '' ? undefined: $('#username').val().trim();
+    var password = $('#password').val().trim() == '' ? undefined: $('#password').val().trim();
+    var request = {};
+    request.username = username;
+    request.password = password;
+    $.ajax({
+        url: this.action,
+        type: this.method,
+        dataType: 'JSON',
+        data: request,
+        success: res => {
+            $('#btn-icon').attr('class', 'fa fa-check');
+            $('blockquote')
+                .attr('class', 'success')
+                .text(res.message);
+            location.reload();
+        },
+        error: e => {
+            $('#btn-icon').attr('class', 'fa fa-exclamation');
+            if (e.responseJSON) {
+                $('blockquote')
+                    .attr('class', 'danger')
+                    .text(e.responseJSON.message);
+            } else {
+                $('blockquote')
+                    .attr('class', 'danger')
+                    .text(`Error ${e.status}: ${e.statusText}`);
+            }
+        },
+        complete: function() {
+            setTimeout(() => {
+                $('#btn-icon').attr('class', 'fa fa-user');
+                $('blockquote')
+                    .attr('class', '')
+                    .text('Ingrese sus credenciales para iniciar sesión');
+            }, 2500);
+        }
+    })
+})
---
+++ admin/login.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+$i = uniqid();
+if (isset($_GET['logout'])) {
+  session_unset();
+  session_destroy();
+  header('location: ./login.php');
+}
+if (
+  isset($_SESSION['status']) &&
+  $_SESSION['status'] == true
+) {
+  header('location: ./');
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Administración | Login</title>
+  <link rel="stylesheet" href="../assets/css/fontawesome.css">
+  <script src="../assets/js/fontawesome.min.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets/css/login.css?<?php echo $i; ?>">
+</head>
+
+<body>
+  <form id="login" action="../api/admin/access" method="POST" autocomplete="off">
+    <img src="../assets/img/lock.svg" alt="Iniciar sesión en SHOP">
+    <h1>Administración | Login</h1>
+    <label for="username">Usuario o correo electrónico</label>
+    <input type="text" id="username" placeholder="ejemplo@dominio.com">
+    <label for="password">Contraseña</label>
+    <input type="password" id="password" placeholder="••••••••••">
+    <blockquote>
+      Ingrese sus credenciales para iniciar sesión
+    </blockquote>
+    <button type="submit">
+      <i id="btn-icon" class="fa fa-user"></i>
+      Acceder
+    </button>
+  </form>
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="assets/js/login.js?<?php echo $i; ?>"></script>
+</body>
+
+</html>
