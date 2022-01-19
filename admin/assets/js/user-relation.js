0815711d005dafaff54affeae94d2a93723fdc61
Najhely Gutierrez <najhelygutierrezcastro@gmail.com>
2022-01-18 22:28:08 -0500

    naty_

--- admin/assets/js/user-relation.js
+++ admin/assets/js/user-relation.js
@@ -1,121 +1,44 @@
-$(function() {
-    'use strict';
-
-    
-    var doughnutPieData = {
-      datasets: [{
+$(function () {
+  "use strict";
+  var doughnutPieData = {
+    datasets: [
+      {
         data: [30, 40, 30],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)',
-          'rgba(255, 159, 64, 0.5)'
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
         ],
         borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
-      }],
-  
-      labels: [
-        'Empresas',
-        'Clientes',
-        'Marketeros',
-      ]
-    };
-    var doughnutPieOptions = {
-      responsive: true,
-      animation: {
-        animateScale: true,
-        animateRotate: true
-      }
-    };  
-    // Get context with jQuery - using jQuery's .get() method.
-    if ($("#barChart").length) {
-      var barChartCanvas = $("#barChart").get(0).getContext("2d");
-      // This will get the first returned node in the jQuery collection.
-      var barChart = new Chart(barChartCanvas, {
-        type: 'bar',
-        data: data,
-        options: options
-      });
-    }
-  
-    if ($("#lineChart").length) {
-      var lineChartCanvas = $("#lineChart").get(0).getContext("2d");
-      var lineChart = new Chart(lineChartCanvas, {
-        type: 'line',
-        data: data,
-        options: options
-      });
-    }
-  
-    if ($("#linechart-multi").length) {
-      var multiLineCanvas = $("#linechart-multi").get(0).getContext("2d");
-      var lineChart = new Chart(multiLineCanvas, {
-        type: 'line',
-        data: multiLineData,
-        options: options
-      });
-    }
-  
-    if ($("#areachart-multi").length) {
-      var multiAreaCanvas = $("#areachart-multi").get(0).getContext("2d");
-      var multiAreaChart = new Chart(multiAreaCanvas, {
-        type: 'line',
-        data: multiAreaData,
-        options: multiAreaOptions
-      });
-    }
-  
-    if ($("#doughnutChart").length) {
-      var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
-      var doughnutChart = new Chart(doughnutChartCanvas, {
-        type: 'doughnut',
-        data: doughnutPieData,
-        options: doughnutPieOptions
-      });
-    }
-  
-    if ($("#pieChart").length) {
-      var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-      var pieChart = new Chart(pieChartCanvas, {
-        type: 'pie',
-        data: doughnutPieData,
-        options: doughnutPieOptions
-      });
-    }
-  
-    if ($("#areaChart").length) {
-      var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
-      var areaChart = new Chart(areaChartCanvas, {
-        type: 'line',
-        data: areaData,
-        options: areaOptions
-      });
-    }
-  
-    if ($("#scatterChart").length) {
-      var scatterChartCanvas = $("#scatterChart").get(0).getContext("2d");
-      var scatterChart = new Chart(scatterChartCanvas, {
-        type: 'scatter',
-        data: scatterChartData,
-        options: scatterChartOptions
-      });
-    }
-  
-    if ($("#browserTrafficChart").length) {
-      var doughnutChartCanvas = $("#browserTrafficChart").get(0).getContext("2d");
-      var doughnutChart = new Chart(doughnutChartCanvas, {
-        type: 'doughnut',
-        data: browserTrafficData,
-        options: doughnutPieOptions
-      });
-    }
-  });
+      },
+    ],
+
+    labels: ["Empresas", "Clientes", "Marketeros"],
+  };
+  var doughnutPieOptions = {
+    responsive: true,
+    animation: {
+      animateScale: true,
+      animateRotate: true,
+    },
+  };
+
+  if ($("#doughnutChart").length) {
+    var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
+    var doughnutChart = new Chart(doughnutChartCanvas, {
+      type: "doughnut",
+      data: doughnutPieData,
+      options: doughnutPieOptions,
+    });
+  }
+});
