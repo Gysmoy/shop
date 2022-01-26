b78002c59d05ca77d42f5cda8bf65f389a29810a
Najhely Gutierrez <najhelygutierrezcastro@gmail.com>
2022-01-26 14:15:51 -0500

    naty_

--- admin/assets/js/login.js
+++ admin/assets/js/login.js
@@ -22,32 +22,29 @@ $('#login').submit(function(form){
         type: this.method,
         dataType: 'JSON',
         data: request,
-        success: res => {
-            $('#btn-icon').attr('class', 'fa fa-check');
+    }).done(function(res) {
+        $('#btn-icon').attr('class', 'fa fa-check');
+        $('blockquote')
+            .attr('class', 'success')
+            .text(res.message);
+        location.reload();
+    }).fail(function(e){
+        $('#btn-icon').attr('class', 'fa fa-exclamation');
+        if (e.responseJSON) {
             $('blockquote')
-                .attr('class', 'success')
-                .text(res.message);
-            location.reload();
-        },
-        error: e => {
-            $('#btn-icon').attr('class', 'fa fa-exclamation');
-            if (e.responseJSON) {
-                $('blockquote')
-                    .attr('class', 'danger')
-                    .text(e.responseJSON.message);
-            } else {
-                $('blockquote')
-                    .attr('class', 'danger')
-                    .text(`Error ${e.status}: ${e.statusText}`);
-            }
-        },
-        complete: function() {
-            setTimeout(() => {
-                $('#btn-icon').attr('class', 'fa fa-user');
-                $('blockquote')
-                    .attr('class', '')
-                    .text('Ingrese sus credenciales para iniciar sesión');
-            }, 2500);
+                .attr('class', 'danger')
+                .text(e.responseJSON.message);
+        } else {
+            $('blockquote')
+                .attr('class', 'danger')
+                .text(`Error ${e.status}: ${e.statusText}`);
         }
+    }).always(function() {
+        setTimeout(() => {
+            $('#btn-icon').attr('class', 'fa fa-user');
+            $('blockquote')
+                .attr('class', '')
+                .text('Ingrese sus credenciales para iniciar sesión');
+        }, 2500);
     })
 })
