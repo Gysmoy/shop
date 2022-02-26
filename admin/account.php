0bf61b0e79a91ec914f410cb9619d8654bb02ef7
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-02-26 18:58:58 -0500

    gambo

--- admin/account.php
+++ admin/account.php
@@ -311,15 +311,15 @@ $version = uniqid(); ?>
   <script src="assets/vendors/progressbar.js/progressbar.min.js"></script>
   <script src="assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
   <script src="assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-  <script src="assets/js/jquery.cookie.js" type="text/javascript"></script>
+  <script src="assets/js/util/jquery.cookie.js" type="text/javascript"></script>
   <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="assets/js/off-canvas.js"></script>
-  <script src="assets/js/hoverable-collapse.js"></script>
-  <script src="assets/js/misc.js"></script>
-  <script src="assets/js/settings.js"></script>
-  <script src="assets/js/todolist.js"></script>
-  <script src="assets/js/dragula.js"></script>
+  <script src="assets/js/util/off-canvas.js"></script>
+  <script src="assets/js/util/hoverable-collapse.js"></script>
+  <script src="assets/js/util/misc.js"></script>
+  <script src="assets/js/util/settings.js"></script>
+  <script src="assets/js/util/todolist.js"></script>
+  <script src="assets/js/util/dragula.js"></script>
   <!-- endinject -->
   <!-- Custom js for this page -->
   <script src="../assets/js/moment.min.js"></script>
